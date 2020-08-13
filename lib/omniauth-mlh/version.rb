316f09501a5ef6baf790e6d12e0cab77d5faabe2
Niko <niko@mlh.io>
2020-08-12 22:25:59 -0400

    Update data returned

--- lib/omniauth-mlh/version.rb
+++ lib/omniauth-mlh/version.rb
@@ -1,7 +1,7 @@
 module OmniAuth
   module MLH
     MAJOR_VERSION = 0
-    MINOR_VERSION = 3
+    MINOR_VERSION = 4
     RELEASE_VERSION = 1
 
     VERSION = [MAJOR_VERSION, MINOR_VERSION, RELEASE_VERSION].join('.')
